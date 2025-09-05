6e16f83c2afdefbe9dc7f4c56666ec6d0b371fd1
corey-db-code <corey@diamond-banc.com>
2025-09-05 18:29:44 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,12 @@
 const SHEET_CONFIG = {
     // Replace 'YOUR_SHEET_ID' with your actual Google Sheets ID
     // You can find this in your sheet URL: https://docs.google.com/spreadsheets/d/YOUR_SHEET_ID/edit
-    SHEET_ID: 'YOUR_SHEET_ID_HERE',
+    SHEET_ID: '1TCOQyfLjfYxWewWEAry45AFlOtpuE3hf6AwqIz-hR-Y',
     
     // Replace 'Sheet1' with your actual sheet name if different
-    SHEET_NAME: 'Sheet1',
+    SHEET_NAME: 'Current Watch Inventory',
     
     // Range to fetch (A:C means columns A, B, and C)
     RANGE: 'A:C'
+
 };
